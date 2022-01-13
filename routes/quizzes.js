ed31846afb6254e432b0b6bc9783350e3c51fe4c
Hamid Nur <hamidsharif2001@gmail.com>
2022-01-12 19:56:51 -0500

    add question

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -14,6 +14,18 @@ module.exports = (db) => {
       });
   });
 
+  // adding a question after creating the quiz
+
+  router.get("/:quizid/questions", (req, res) => {
+    req.session.quiz_id = req.params.quizid;
+    db.query(``, [req.params.quizid])
+    .then(data => {
+      let templateVar = { quizId: req.params.quizid, user: data.rows[0] };
+      res.render('../views/questions', templateVar);
+    })
+  })
+
+
 
   return router;
 };
