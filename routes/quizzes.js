bc6c090b0542ce4f96a53f45e8fc6836dfd064f9
Hamid Nur <hamidsharif2001@gmail.com>
2022-01-12 19:55:03 -0500

    quiz in progress

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const router = express.Router();
+
+
+module.exports = (db) => {
+
+  // quiz in progress page, users can still add questions
+
+  router.get('/:quizid', (req, res) => {
+    db.query(``, [req.params.quizid])
+      .then(data => {
+        let templateVar = { input: data.rows }
+        res.render('../views/quiz_in_prog', templateVar)
+      });
+  });
+
+
+  return router;
+};
