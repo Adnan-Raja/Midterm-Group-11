f030e5f37da26089b9bb9e23b5dc78678dde76e5
Hamid Nur <hamidsharif2001@gmail.com>
2022-01-13 21:01:46 -0500

    add answers + installed cookie parser

--- routes/questions.js
+++ routes/questions.js
@@ -13,4 +13,36 @@ module.exports = (db) => {
         res.render('../views/questions', templateVars);
       });
   });
+<<<<<<< Updated upstream
 }
+=======
+
+  // adding the inputted answers to the database
+
+  router.post('/:questionId', (req, res) => {
+    let values = [req.params.questionId, req.body.answer1, req.body.isCorrecta1, req.body.answer2, req.body.isCorrecta2,
+    req.body.answer3, req.body.isCorrecta3, req.body.answer4, req.body.isCorrecta4,];
+    let query = `
+        INSERT INTO answers (question_id, answer, isCorrect)
+        VALUES
+            ($1, $2, $3), ($1, $4, $5), ($1, $6, $7), ($1, $8, $9)
+        RETURNING *;
+        `;
+    db.query(query, values)
+      .then(async(data) => {
+        const questionID = await(data.rows[0].question_id);
+        db.query(`
+          SELECT quiz_id FROM questions WHERE questions.id = $1;
+        `, [questionID])
+        .then(async(response) => {
+          let quizid = await(response.rows[0].quiz_id);
+          res.redirect(`/quiz/${quizid}`);
+        })
+      })
+      .catch(err => {
+        res.send(`Please click back and complete the form.`);
+      });
+  });
+  return router;
+};
+>>>>>>> Stashed changes
